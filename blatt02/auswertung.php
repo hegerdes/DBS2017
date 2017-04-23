98d06bba24d69d7e95b4acfbae7a6e361141e7f5
hegerdes <hegerdes@uni-osnabrueck.de>
2017-04-23 23:19:33 +0200

    Updated File

--- blatt02/auswertung.php
+++ blatt02/auswertung.php
@@ -15,27 +15,39 @@ $neu = str_word_count($content);
 $rows = explode("\n", $content);
 
 $anzahl = 0;
-$az = 0;
 $durchgefallen = 0;
-$schnitt = 2.0;
+$schnitt = 0.0;
 $min = 1000.0;
+$noten = array();
 
+// Test der Dateitypen
+/*
+var_dump($schnitt);
+var_dump($anzahl);
+var_dump($durchgefallen);
+var_dump($min);
+*/
 
 // Über alle Zeilen iterieren
 foreach ($rows as $row) {
 
     // Zeile in Wörter zerlegen
     $words = explode(",", $row);
-	$az++;
 
     // Über alle Wörter der Zeile iterieren
     foreach ($words as $word) {
 		// Alle "Wörter" ausgeben
-		printf("String Inhalt von word ist %d in der Datei \n", $word);
+//		printf("String Inhalt von word ist %d in der Datei \n", $word);
 		
 		// Alle "Wörter" ausgeben
-		$schnitt = $word;
-		printf("Floadt Inhalt von Schnitt ist %d in Datei \n", $schnitt);
+		//$schnitt = $word;
+		//printf("Floadt Inhalt von Schnitt ist %d in Datei \n", $schnitt);
+		//echo $schnitt;
+		
+		// Lege alle Noten als Float in ein Array
+		if ($word < 10 and $word > 0){
+			$noten[$anzahl] = (float)$word;
+		}
 		
         //  Anzahl der Noten zählen
         if ($word > 10) {
@@ -45,18 +57,36 @@ foreach ($rows as $row) {
 		if ($word < 10 and $word > 4){
 			$durchgefallen++;
 		}
-		// Suche Minimum - klappt noch nicht
-		if ($word < $min){
-			$min = $word;
-		}
+		
+		
     }
 }
 
-// Testet Datentyp
-var_dump($schnitt);
+for($i = 1; $i < 42; $i++) {
+	if ($noten[$i] < $min){
+		$min = $noten[$i];
+	}
+    $schnitt = $schnitt + $noten[$i];
+}
+$schnitt = $schnitt/$anzahl;
 
-// Gabe alles auf Konsle aus
-printf("Es haben " . $anzahl . " Studenten an  der Klausur teilgenommen. Davon haben " . $durchgefallen . " nicht bestanden. 
-Die beste Note ist " . $min . "Die Daten stammen aus der Datei \"%s\".\n", $path);
+// Testet Datentyp
+/*
+var_dump($schnitt);
+var_dump($anzahl);
+var_dump($durchgefallen);
+var_dump($min);
+echo count($noten);
+echo "Array an Stelle 0 ist " . $noten[0] . " und an STelle5 " . $noten[5];
+echo $schnitt;
+*/
+
+// Gebe alles auf Konsle aus
+printf("Es haben " . $anzahl . " Studenten an  der Klausur teilgenommen. \n 
+Davon haben " . $durchgefallen . " nicht bestanden. \n
+Der Durchscnitt ist " . $schnitt . ". \n 
+Die beste Note ist " . $min . ". \n
+Die Daten stammen aus der Datei \"%s\".\n", $path);
+
 
 ?>
